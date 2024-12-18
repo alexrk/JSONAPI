79a6b2203c7d7d9536d475055199a29ce826bedb
Dmitry Neveshkin <dmitry@Dmitrys-MacBook-Pro.local>
2024-12-18 08:34:09 +0100

    add CodablePrimaryResource support for Poly16

--- Sources/JSONAPI/Document/AdditionalIncludes.swift
+++ Sources/JSONAPI/Document/AdditionalIncludes.swift
@@ -14,3 +14,44 @@ extension Includes where I: _Poly16 {
         return values.compactMap(\.p)
     }
 }
+
+// MARK: - 15 types
+extension Poly16: EncodablePrimaryResource, OptionalEncodablePrimaryResource
+where
+A: EncodablePolyWrapped,
+B: EncodablePolyWrapped,
+C: EncodablePolyWrapped,
+D: EncodablePolyWrapped,
+E: EncodablePolyWrapped,
+F: EncodablePolyWrapped,
+G: EncodablePolyWrapped,
+H: EncodablePolyWrapped,
+I: EncodablePolyWrapped,
+J: EncodablePolyWrapped,
+K: EncodablePolyWrapped,
+L: EncodablePolyWrapped,
+M: EncodablePolyWrapped,
+N: EncodablePolyWrapped,
+O: EncodablePolyWrapped,
+P: EncodablePolyWrapped
+{}
+
+extension Poly16: CodablePrimaryResource, OptionalCodablePrimaryResource
+where
+A: CodablePolyWrapped,
+B: CodablePolyWrapped,
+C: CodablePolyWrapped,
+D: CodablePolyWrapped,
+E: CodablePolyWrapped,
+F: CodablePolyWrapped,
+G: CodablePolyWrapped,
+H: CodablePolyWrapped,
+I: CodablePolyWrapped,
+J: CodablePolyWrapped,
+K: CodablePolyWrapped,
+L: CodablePolyWrapped,
+M: CodablePolyWrapped,
+N: CodablePolyWrapped,
+O: CodablePolyWrapped,
+P: CodablePolyWrapped
+{}
